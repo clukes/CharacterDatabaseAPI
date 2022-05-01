018de61bcba03da83f6ed4bea33d13e231193393
clukes <clukes@me.com>
2022-05-01 13:16:44 +0100

    Refactoring database structure

--- Models/CharacterDatabaseSettings.cs
+++ Models/CharacterDatabaseSettings.cs
@@ -3,9 +3,10 @@ namespace CharacterDatabaseAPI.Models
     public class CharacterDatabaseSettings
     {
         public string ConnectionString { get; set; } = null!;
-
         public string DatabaseName { get; set; } = null!;
-
         public string CharacterCollectionCollectionName { get; set; } = null!;
+        public string CategoryTypeCollectionSuffix { get; set; } = null!;
+        public string CharacterCollectionSuffix { get; set; } = null!;
+        public string[] CollectionPrefixes { get; set; } = null!;
     }
 }
